1c7c5f919924b7eb529a31da88b86340db21116b
Bulent Kaya <97361091+buelentkaya@users.noreply.github.com>
2022-03-31 23:01:47 +0200

    initial commit

--- src/Git_Day_1/HelloWorld/HelloWorld.java
+++ src/Git_Day_1/HelloWorld/HelloWorld.java
@@ -1,4 +1,7 @@
 package Git_Day_1.HelloWorld;
 
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("I have committed my own first Command! ");
+    }
 }
