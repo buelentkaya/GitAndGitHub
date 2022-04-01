14e44078a9dcc9d7cd4a58f4ca9fe6ae3da416a0
Bulent Kaya <97361091+buelentkaya@users.noreply.github.com>
2022-04-01 17:56:45 +0200

    initial commit

--- src/Git_Day_1/HelloWorld/HelloWorld.java
+++ src/Git_Day_1/HelloWorld/HelloWorld.java
@@ -3,5 +3,11 @@ package Git_Day_1.HelloWorld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("I have committed my own first Command! ");
+
+        // vaaoouuuvvvvv Git issue is easier than i expected...which is a kidplay.
+
+
+
+
     }
 }
