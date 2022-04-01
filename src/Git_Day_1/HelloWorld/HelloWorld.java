8fa2cd62fe5a6fd081960144aa8a00d8187de701
Bulent Kaya <97361091+buelentkaya@users.noreply.github.com>
2022-04-01 19:47:18 +0200

    work 3 is done and about to be pushed to Github after commit

--- src/Git_Day_1/HelloWorld/HelloWorld.java
+++ src/Git_Day_1/HelloWorld/HelloWorld.java
@@ -7,7 +7,11 @@ public class HelloWorld {
         // vaaoouuuvvvvv Git issue is easier than i expected...which is a kids' play.
 
 
-// doing awsemo work I have been doing :-)
+// doing awesome work I have been doing :-)
+
+        // Doing awesome work 3!
+        // Awesome work 3 is done and ready to be committed//and ready to be pushed to remote repository
+
 
     }
 }
