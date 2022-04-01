9603cbd1229ab2f8d0842b35be5d12ba2901083e
Bulent Kaya <97361091+buelentkaya@users.noreply.github.com>
2022-04-01 18:06:11 +0200

    work 2 is done

--- src/Git_Day_1/HelloWorld/HelloWorld.java
+++ src/Git_Day_1/HelloWorld/HelloWorld.java
@@ -4,10 +4,10 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("I have committed my own first Command! ");
 
-        // vaaoouuuvvvvv Git issue is easier than i expected...which is a kidplay.
+        // vaaoouuuvvvvv Git issue is easier than i expected...which is a kids' play.
 
 
-
+// doing awsemo work I have been doing :-)
 
     }
 }
